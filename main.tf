161abaf833e9d786af27661eb66dfdb582c233e5
kailsajan <kailsajan31@gmail.com>
2020-08-28 17:09:57 +0800

    firstcode

---
+++ main.tf
@@ -0,0 +1,22 @@
+# Configure the Azure Provider
+provider "azurerm" {
+  subscription_id = var.azure-subscription-id
+  client_id       = var.azure-client-id
+  client_secret   = var.azure-client-secret 
+  tenant_id       = var.azure-tenant-id
+}
+resource "azurerm_resource_group" "shane" {
+  name     = "shane"
+  location = "southeast asia"
+}
+
+resource "azurerm_storage_account" "shanestrtacnt" {
+  name                     = "shanestrtacnt"
+  resource_group_name      = azurerm_resource_group.shane.name
+  location                 = azurerm_resource_group.shane.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+  tags = {
+    environment = "staging"
+  }
+}
